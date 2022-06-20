7f94593db3b48553b4fee11a55662409cba28b63
Pilate <noptical@gmail.com>
2022-06-19 18:55:38 -0700

    more eps

--- extension/js/vskipdb.js
+++ extension/js/vskipdb.js
@@ -34,5 +34,51 @@ const VSKIPDB = {
         "credits": [
             [1287]
         ]
+    },
+    "198465": {
+        "_description": "The Office - S01E05",
+        "intro": [
+            [0, 32]
+        ],
+        "credits": [
+            [1292]
+        ]
+    },
+    "198469": {
+        "_description": "The Office - S01E06",
+        "intro": [
+            [0, 32]
+        ],
+        "credits": [
+            [1294]
+        ]
+    },
+
+    "198474": {
+        "_description": "The Office - S02E01",
+        "intro": [
+            [0, 33]
+        ],
+        "credits": [
+            [1221]
+        ]
+    },
+    "198481": {
+        "_description": "The Office - S02E02",
+        "intro": [
+            [0, 32]
+        ],
+        "credits": [
+            [1261]
+        ]
+    },
+    "198485": {
+        "_description": "The Office - S02E03",
+        "intro": [
+            [48, 81]
+        ],
+        "credits": [
+            // [1261]
+        ]
     }
 }
